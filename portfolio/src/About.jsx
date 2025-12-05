778658280e02132a07934d2d89f011f09c20a178
SolidSoups <eli.marc.brown@gmail.com>
2025-12-05 13:06:03 +0100

    changed some things

--- portfolio/src/About.jsx
+++ portfolio/src/About.jsx
@@ -3,28 +3,29 @@ import { SECTIONS } from './constants';
 
 export default function About({ onNavigate }) {
     return (
-        <>
+        <div className="profile-container">
+            <div>
+                <p>
+                    My name is <b>Elias Brown</b>. I am a 22 year old software
+                    engineer based in Malmö, Sweden.
+                </p>
+                <p>
+                    Currently studying <b>Game Engine & Tools Programming</b> at{' '}
+                    <a
+                        href="https://futuregames.se/se/locations/malmo/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        FutureGames Malmö
+                    </a>
+                    .
+                </p>
+            </div>
             <img
                 src="/images/profile.JPG"
                 alt="Elias Brown profile picture"
                 className="profile-pic"
             />
-            <p>
-                My name is <b>Elias Brown</b>. I am a 22 year old software
-                engineer based in Malmö, Sweden.
-            </p>
-            <p>
-                Currently studying <b>Game Engine & Tools Programming</b> at{' '}
-                <a
-                    href="https://futuregames.se/se/locations/malmo/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    FutureGames Malmö
-                </a>
-                .
-            </p>
-            <br />
-        </>
+        </div>
     );
 }
